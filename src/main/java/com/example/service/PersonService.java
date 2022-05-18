8d68708bbb21fe006004b3fbf4e3c6754d88fbfe
Mohammed Mansour <mohammedmansournseer@gmail.com>
2022-05-19 01:50:46 +0200

    add new person for test

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("4");
+		p.setAge(25);
+		p.setFirstName("Webhook");
+		p.setLastName("test");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
